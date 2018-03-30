094b7d92f96d088761e0bc622786a167f1304c11
f18m <francesco.montorsi@gmail.com>
2018-03-30 21:54:52 +0200

    Create a structure more Python-oriented

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='python-rpm-spec-dependency-analyzer',
+    packages=['pyrpm-dependency-analyzer'],
+    version='0.1',
+    description='Module for analyzing RPM spec dependencies.',
+    author='Francesco Montorsi',
+    author_email='francesco.montorsi@gmail.com',
+    url='https://github.com/f18m/rpm-spec-dependency-analyzer',
+    download_url='  # use the URL to the GitHub repo/archive/0.1.tar.gz',
+    keywords=['RPM', 'spec', 'graphviz', 'dependencies', 'dependency-analysis'],
+    classifiers=[],
+)
