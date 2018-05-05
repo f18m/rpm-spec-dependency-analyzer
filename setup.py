02c856246d198b7064ef5e4ac8f7515b182f69c8
f18m <francesco.montorsi@gmail.com>
2018-05-05 18:48:00 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,37 @@
-from distutils.core import setup
+#!/usr/bin/python
+# see https://pypi.org/project/python-rpm-spec-dependency-analyzer/
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='python-rpm-spec-dependency-analyzer',
     packages=['pyrpm-dependency-analyzer'],
-    version='0.1',
+    version='0.2',
     description='Module for analyzing RPM spec dependencies.',
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
     author='Francesco Montorsi',
     author_email='francesco.montorsi@gmail.com',
     url='https://github.com/f18m/rpm-spec-dependency-analyzer',
     download_url='https://github.com/f18m/rpm-spec-dependency-analyzer/archive/1.0.tar.gz',
     keywords=['RPM', 'spec', 'graphviz', 'dependencies', 'dependency-analysis'],
-    classifiers=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
 )
